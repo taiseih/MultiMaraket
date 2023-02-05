f01025bdd4849004937bc9531400d04c35662dcd
taiseih <taisei.hayashizaki@gmail.com>
2023-02-05 22:28:02 +0900

    見た目の変更

--- routes/admin.php
+++ routes/admin.php
@@ -25,9 +25,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('admin.welcome');
-// });
+Route::get('/', function () {
+    return view('admin.welcome');
+});
 
 Route::resource('owners', OwnersController::class)
 ->middleware('auth:admin')
