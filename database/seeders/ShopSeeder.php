fc72ce1435e762a292553211d39e76b3adfd21de
taiseih <taisei.hayashizaki@gmail.com>
2023-02-06 02:35:14 +0900

    shopとownerのリレーション

---
+++ database/seeders/ShopSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ShopSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('shops')->insert([
+            [
+                'owner_id' => 1,
+                'name' => '店名が入ります',
+                'information' => '店舗情報が入ります。店舗情報が入ります。店舗情報が入ります。',
+                'filename' => '',
+                'is_selling' => true,
+                'created_at' => '2023/1/1 11:11:11'
+            ],
+            [
+                'owner_id' => 2,
+                'name' => '店名が入ります',
+                'information' => '店舗情報が入ります。店舗情報が入ります。店舗情報が入ります。',
+                'filename' => '',
+                'is_selling' => true,
+                'created_at' => '2023/1/1 11:11:11'
+                ], 
+        ]);
+    }
+}
