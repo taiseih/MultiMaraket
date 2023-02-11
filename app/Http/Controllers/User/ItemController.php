dbc243bfbd6aaa1e9e946794020244f8c9852330
taiseih <taisei.hayashizaki@gmail.com>
2023-02-11 15:23:24 +0900

    index画面調整

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
     public function index(){
-        return view('user.items.index');
+        $products = Product::all();
+        return view('user.items.index', compact('products'));
     }
 }
