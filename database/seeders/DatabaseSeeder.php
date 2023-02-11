8a6d5ecb39b3b3bb0874776b54bc0adc3c53dc84
taiseih <taisei.hayashizaki@gmail.com>
2023-02-11 15:14:22 +0900

    userIndex

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,6 +29,7 @@ class DatabaseSeeder extends Seeder
             ImageSeeder::class,
             // ProductSeeder::class,
             // StockSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'ユーザー1',
+                'email' => 'user0@user.com',
+                'password' => Hash::make('password123'),
+                'created_at' => '2023/1/1 11:11:11'
+            ]
+            ]);
+    }
+}
